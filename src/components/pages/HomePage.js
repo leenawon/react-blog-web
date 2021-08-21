660ca44001a7e9597d111e2f2f7e588e3e6d7524
leenawon <nawon5154@gmail.com>
2021-08-22 01:33:24 +0900

    HomePage.js page component 추가

---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../pages/HomePage.css';
+import HeroImage from '../HeroImage/HeroImage';
+
+function HomePage() {
+  return (
+    <>
+      {/* Hero Image component */}
+      <HeroImage></HeroImage>
+    </>
+  )
+}
+
+export default HomePage;

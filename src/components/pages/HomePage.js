93520a4099c284148f04788675998c62097f7b53
leenawon <nawon5154@gmail.com>
2021-08-28 02:18:54 +0900

    HomePage.js에 BlogPost component 추가

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import '../pages/HomePage.css';
 import HeroImage from '../HeroImage/HeroImage';
+import BlogPost from '../BlogPost/BlogPosts';
 
 function HomePage() {
   return (
     <>
       {/* Hero Image component */}
       <HeroImage></HeroImage>
+      {/* Home Page */}
+      <div className="home-page">
+        {/* Blog Post component */}
+        <BlogPost></BlogPost>
+      </div>
     </>
   )
 }

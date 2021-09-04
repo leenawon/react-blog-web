1c253af273df898e7231f9c4fc32baeaee53d384
leenawon <nawon5154@gmail.com>
2021-09-04 20:23:06 +0900

    App.js에 ShowPost component 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import TopNavBar from './components/TopNavBar/TopNavBar';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
+import ShowPost from './components/ShowPost/ShowPost';
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
         <Switch>
           {/* Home Page component */}
           <Route exact path="/" component={HomePage}></Route>
+          {/* Show Post component */}
+          <Route exact path="/show-post" component={ShowPost}></Route>
         </Switch>
       </BrowserRouter>
     </>

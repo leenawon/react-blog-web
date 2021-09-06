426d57d7a59c6f6d06af300880bc212b3031712c
leenawon <nawon5154@gmail.com>
2021-09-07 01:52:49 +0900

    App.js에 WritePost component route 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import TopNavBar from './components/TopNavBar/TopNavBar';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
 import ShowPost from './components/ShowPost/ShowPost';
+import WritePost from './components/pages/WritePost/WritePost';
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
           <Route exact path="/" component={HomePage}></Route>
           {/* Show Post component */}
           <Route exact path="/show-post" component={ShowPost}></Route>
+          {/* Write Post Component */}
+          <Route exact path="/write-post" component={WritePost}></Route>
         </Switch>
       </BrowserRouter>
     </>

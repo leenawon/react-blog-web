81b38bc78bdb332352980b53d110785894152e69
leenawon <nawon5154@gmail.com>
2021-10-14 01:08:59 +0900

    TopNavBar.js Link 추가 및 수정

--- src/components/TopNavBar/TopNavBar.js
+++ src/components/TopNavBar/TopNavBar.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './TopNavBar.css';
 import profileImage from '../../images/profile-image.jpg';
+import { Link } from 'react-router-dom';
 
 function TopNavBar() {
+  const user = true;
   return (
     // Top NavBar
     <div className="top-navbar">
@@ -15,15 +17,38 @@ function TopNavBar() {
       </div>
       {/* NavBar List */}
       <ul className="navbar-list">
-        <li>홈</li>
-        <li>글쓰기</li>
-        <li>ABOUT</li>
-        <li>CONTACT</li>
-        <li>로그아웃</li>
+        <Link to="/">
+          <li>홈</li>
+        </Link>
+        <Link to="/write-post">
+          <li>글쓰기</li>
+        </Link>
+        <Link to="/user-setting">
+          <li>ABOUT</li>
+        </Link>
+        <Link to="/">
+          <li>CONTACT</li>
+        </Link>
+        {
+          user ? 
+          <li>로그아웃</li>
+          : 
+          <Link to="/login">
+            <li>로그인</li>
+          </Link>
+        }
       </ul>
       {/* Profile Image & Search Icon */}
       <div className="profile-and-search">
-        <img src={profileImage} alt="Profile"></img>
+          {
+            user ? 
+            <Link to="/user-setting">
+              <img src={profileImage} alt="Profile"></img>
+            </Link>
+            : <Link to="/signup">
+                <i className="fas fa-user-circle"></i>
+            </Link>
+          }
         <i className="fas fa-search"></i>
       </div>
     </div>
